3b3e69034054cc84118fcdf87dae26d383d9952d
Administrator <Administrator@WIN7-20160722SS>
2020-02-14 03:33:08 +0900

    커밋/기존파일 삭제, servlet-context 수정

--- src/main/java/com/ateam/solohomes/board/C.java
+++
@@ -1,8 +0,0 @@
-package com.ateam.solohomes.board;
-
-import org.apache.ibatis.session.SqlSession;
-
-public class C {
-	// Mybatis용 sqlSession
-	public static SqlSession sqlSession;
-}
--- src/main/java/com/ateam/solohomes/ticket/ITicketDAO.java
+++
@@ -1,6 +0,0 @@
-package com.ateam.solohomes.ticket;
-
-public interface ITicketDAO {
-	int insertCard(String userID, int buyAmount);
-	int insertTicket(String userID, int ticketCount);
-}
