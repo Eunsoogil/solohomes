5898ac97ab98e96fe2d8d85989505e5892aa11d1
Miji <Miji@DESKTOP-0RQ0LCF>
2020-02-14 16:01:47 +0900

    pull 안되서 커밋

---
+++ src/main/java/com/ateam/solohomes/mypage/controller/MypageController.java
@@ -0,0 +1,12 @@
+package com.ateam.solohomes.mypage.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/mypage")
+public class MypageController {
+
+	
+		
+}
