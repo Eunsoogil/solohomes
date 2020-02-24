8d681eb23cc31aec8d5379ff1548047a375ec49d
pow37 <pow37@DESKTOP-1UVCN15>
2020-02-25 03:10:25 +0900

    박종일 : index.jsp 일간 판매현황 테이블을 위한 dao 작성

--- src/main/java/com/ateam/solohomes/commnad/ManagerIndexCommand.java
+++ src/main/java/com/ateam/solohomes/commnad/ManagerIndexCommand.java
@@ -1,9 +1,12 @@
 package com.ateam.solohomes.commnad;
 
+import java.util.ArrayList;
+
 import org.springframework.ui.Model;
 
 import com.ateam.solohomes.C;
 import com.ateam.solohomes.beans.manager.ManagerDAO;
+import com.ateam.solohomes.beans.manager.MonthlySalesDTO;
 
 public class ManagerIndexCommand implements Command {
 
@@ -15,15 +18,18 @@ public class ManagerIndexCommand implements Command {
 		Integer countOfMembers = dao.countTodaySignUp();
 		Integer countOfSales = dao.countTodaySales();
 		Integer sumOfSales = dao.sumTodaySales();
+		ArrayList<MonthlySalesDTO> list = dao.getMonthlySales();
 		
 		double d = sumOfSales / 10000.0;
-		System.out.println(countOfMembers);
-		System.out.println(countOfSales);
-		System.out.println(sumOfSales);
+		//System.out.println(countOfMembers);
+		//System.out.println(countOfSales);
+		//System.out.println(sumOfSales);
+		System.out.println(list.toString());
 		
 		model.addAttribute("countOfMembers", countOfMembers);
 		model.addAttribute("countOfSales", countOfSales);
 		model.addAttribute("sumOfSales", d);
+		model.addAttribute("mothlySalesList", list);
 	}
 
 }
